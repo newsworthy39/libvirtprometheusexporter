2b288c86785b4084ba605713a198f96272822cff
newsworthy39 <newsworthy39@github.com>
2023-09-18 20:35:27 +0200

    BUGFIX: rename labels

--- include/serializers.hpp
+++ include/serializers.hpp
@@ -22,7 +22,7 @@ namespace serializer
                 std::vector<std::string> fields = custom::split(record->params[k].field);
                 if (fields.size() == 3)
                 {
-                    out.append(custom::format("# TYPE %s_%s counter\n", fields[0].c_str(), fields[2].c_str()));
+                    out.append(custom::format("# TYPE virt_%s_%s counter\n", fields[0].c_str(), fields[2].c_str()));
                     out.append(custom::format("virt_%s_%s{domain=\"%s\", vcpu=\"%s\"} %llu\n",
                                               fields[0].c_str(),
                                               fields[2].c_str(),
@@ -53,7 +53,7 @@ namespace serializer
 
                 if (fields.size() == 4)
                 {
-                    out.append(custom::format("# TYPE %s_%s_%s counter\n", fields[0].c_str(), fields[3].c_str(), fields[2].c_str()));
+                    out.append(custom::format("# TYPE virt_%s_%s_%s counter\n", fields[0].c_str(), fields[3].c_str(), fields[2].c_str()));
                     out.append(custom::format("virt_%s_%s_%s{domain=\"%s\" interfaceid=\"%s\", name=\"%s\"} %llu\n",
                                               fields[0].c_str(), fields[3].c_str(), fields[2].c_str(), // => net_bytes_rx
                                               virDomainGetName(record->dom), fields[1].c_str(), netname.c_str(),
